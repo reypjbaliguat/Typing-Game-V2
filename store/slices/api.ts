ecdec5e9ae78b7ae1fa9efa1a506b6c5b976d654
reypjbaliguat <reypjbaliguat@gmail.com>
2023-09-03 11:59:54 +0800

    store moved to root

---
+++ store/slices/api.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/`,
+    prepareHeaders: async (headers) => {
+      const token = JSON.parse(localStorage.getItem("user") as string)?.token;
+      headers.set("Authorization", `Bearer ${token}`);
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+  tagTypes: ["Score", "User", "Word"],
+});
---
+++ store/slices/auth.ts
@@ -0,0 +1,18 @@
+import { api } from "./api";
+import { User } from "@/types";
+
+const API_PATH = "auth";
+export const authApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    loginViaSocial: builder.mutation<User, { body: Partial<User> }>({
+      query: ({ body }: { body: User }) => ({
+        url: `${API_PATH}/login-social`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
+
+export const { useLoginViaSocialMutation } = authApi;
